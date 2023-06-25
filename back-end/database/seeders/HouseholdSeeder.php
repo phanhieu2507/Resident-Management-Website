11d519c6dcea73a11ed835cf0a94eaa2200753eb
Phan Cong Hieu <phanhieu2572002@gmail.com>
2023-06-25 16:33:27 +0700

    update seed data

--- back-end/database/seeders/HouseholdSeeder.php
+++ back-end/database/seeders/HouseholdSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Household;
+use App\Models\Resident;
+use Faker\Factory as Faker;
 
 class HouseholdSeeder extends Seeder
 {
@@ -14,98 +17,64 @@ class HouseholdSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('households')->insert([
-            [
-                'house_number' => 'Số nhà 1',
-                'street' => 'Đường A',
-                'ward' => 'Phường B',
-                'district' => 'Quận C',
-                'head_of_household' => 'Nguyễn Văn A',
-                'household_size' => 5,
-                'date_of_registration' => '2021-01-01',
-            ],
-            [
-                'house_number' => 'Số nhà 2',
-                'street' => 'Đường X',
-                'ward' => 'Phường Y',
-                'district' => 'Quận Z',
-                'head_of_household' => 'Trần Thị B',
-                'household_size' => 3,
-                'date_of_registration' => '2021-02-15',
-            ],
-            [
-                'house_number' => 'Số nhà 3',
-                'street' => 'Đường M',
-                'ward' => 'Phường N',
-                'district' => 'Quận P',
-                'head_of_household' => 'Lê Thị C',
-                'household_size' => 4,
-                'date_of_registration' => '2021-03-10',
-            ],
-            [
-                'house_number' => 'Số nhà 4',
-                'street' => 'Đường G',
-                'ward' => 'Phường H',
-                'district' => 'Quận I',
-                'head_of_household' => 'Trương Văn D',
-                'household_size' => 2,
-                'date_of_registration' => '2021-04-20',
-            ],
-            [
-                'house_number' => 'Số nhà 5',
-                'street' => 'Đường S',
-                'ward' => 'Phường T',
-                'district' => 'Quận U',
-                'head_of_household' => 'Phạm Thị E',
-                'household_size' => 6,
-                'date_of_registration' => '2021-05-05',
-            ],
-            [
-                'house_number' => 'Số nhà 6',
-                'street' => 'Đường D',
-                'ward' => 'Phường E',
-                'district' => 'Quận F',
-                'head_of_household' => 'Vũ Văn F',
-                'household_size' => 3,
-                'date_of_registration' => '2021-06-18',
-            ],
-            [
-                'house_number' => 'Số nhà 7',
-                'street' => 'Đường K',
-                'ward' => 'Phường L',
-                'district' => 'Quận M',
-                'head_of_household' => 'Ngô Thị G',
-                'household_size' => 5,
-                'date_of_registration' => '2021-07-02',
-            ],
-            [
-                'house_number' => 'Số nhà 8',
-                'street' => 'Đường R',
-                'ward' => 'Phường Q',
-                'district' => 'Quận O',
-                'head_of_household' => 'Đỗ Văn H',
-                'household_size' => 4,
-                'date_of_registration' => '2021-08-14',
-            ],
-            [
-                'house_number' => 'Số nhà 9',
-                'street' => 'Đường W',
-                'ward' => 'Phường X',
-                'district' => 'Quận Y',
-                'head_of_household' => 'Hồ Thị I',
-                'household_size' => 3,
-                'date_of_registration' => '2021-09-29',
-            ],
-            [
-                'house_number' => 'Số nhà 10',
-                'street' => 'Đường B',
-                'ward' => 'Phường A',
-                'district' => 'Quận Z',
-                'head_of_household' => 'Trần Văn K',
-                'household_size' => 7,
-                'date_of_registration' => '2021-10-12',
-            ]
+        $faker = Faker::create();
+
+        // Tạo 10 hộ khẩu
+        for ($i = 0; $i < 10; $i++) {
+            $household = Household::create([
+                'house_number' => $faker->buildingNumber,
+                'street' => $faker->streetName,
+                'ward' => $faker->citySuffix,
+                'district' => $faker->city,
+                'head_of_household' => 0,
+                'household_size' => 0,
+                'date_of_registration' => $faker->date(),
+            ]);
+             $head = Resident::create([
+                'household_id' => $household->id,
+                'full_name' => $faker->name,
+                'gender' => $faker->randomElement(['Nam', 'Nữ']),
+                'date_of_birth' => $faker->date(),
+                'place_of_birth' => $faker->city,
+                'ethnicity' => $faker->randomElement(['Kinh', 'Tày', 'Nùng', 'Hoa', 'Khác']),
+                'occupation' => $faker->jobTitle,
+                'id_card_number' => $faker->unique()->numerify('############'),
+                'date_of_issue' => $faker->date(),
+                'place_of_issue' => $faker->city,
+                'status' => $faker->randomElement(['Thường trú', 'Tạm trú']),
+                'date_of_register' => $faker->date(),
+                'current_address' => $faker->address,
+                'previous_address' => $faker->address,
+                'relationship_with_head' => 'chủ hộ',
+            ]);
             
-        ]);
+                $household->head_of_household = $head->id;
+                $household->household_size = 5;
+                $household->save();
+            
+            // Tạo 5 thành viên trong mỗi hộ khẩu
+            for ($j = 0; $j < 4; $j++) {
+                $relationship = $faker->randomElement([ 'Con', 'Vợ', 'Chồng', 'Anh/Chị/Em']);
+                $resident = Resident::create([
+                    'household_id' => $household->id,
+                    'full_name' => $faker->name,
+                    'gender' => $faker->randomElement(['Nam', 'Nữ']),
+                    'date_of_birth' => $faker->date(),
+                    'place_of_birth' => $faker->city,
+                    'ethnicity' => $faker->randomElement(['Kinh', 'Tày', 'Nùng', 'Hoa', 'Khác']),
+                    'occupation' => $faker->jobTitle,
+                    'id_card_number' => $faker->unique()->numerify('############'),
+                    'date_of_issue' => $faker->date(),
+                    'place_of_issue' => $faker->city,
+                    'status' => $faker->randomElement(['Thường trú', 'Tạm trú']),
+                    'date_of_register' => $faker->date(),
+                    'current_address' => $faker->address,
+                    'previous_address' => $faker->address,
+                    'relationship_with_head' => $relationship,
+                ]);
+
+                
+            }
+        }    
     }
 }
--- front-end/src/components/home/HouseholdDetailsModal.js
+++ front-end/src/components/home/HouseholdDetailsModal.js
@@ -4,45 +4,54 @@ import { Modal, Steps, Card, Button } from "antd";
 const HouseholdDetailsModal = ({ selectedHousehold, modalVisible, handleModalClose }) => {
   const [currentPage, setCurrentPage] = useState(0);
   const { Step } = Steps;
+
+  // Kiểm tra selectedHousehold và residents trước khi truy cập
+  const residents = selectedHousehold?.residents?.slice() || [];
+
+  // Đặt chủ hộ lên đầu danh sách thành viên
+  const headOfHouseholdIndex = residents.findIndex((resident) => resident.id === selectedHousehold?.head_of_household);
+  if (headOfHouseholdIndex !== -1) {
+    const headOfHousehold = residents.splice(headOfHouseholdIndex, 1)[0];
+    residents.unshift(headOfHousehold);
+  }
+
   const renderHouseholdInfoPage = () => {
     return (
-        
       <Card title="Thông tin hộ khẩu">
         {/* Hiển thị thông tin của hộ khẩu */}
-        <p>ID: {selectedHousehold.id}</p>
-        <p>Số Nhà: {selectedHousehold.house_number}</p>
-        <p>Đường: {selectedHousehold.street}</p>
-        <p>Phường/Xã: {selectedHousehold.ward}</p>
-        <p>Quận/Huyện: {selectedHousehold.district}</p>
-        <p>Chủ Hộ: {selectedHousehold.residents.find((resident) => resident.id === selectedHousehold.head_of_household).full_name}</p>
-        <p>Kích Thước Hộ Gia Đình: {selectedHousehold.household_size}</p>
-        <p>Ngày Đăng Ký Hộ Khẩu: {selectedHousehold.date_of_registration}</p>
+        <p>ID: {selectedHousehold?.id}</p>
+        <p>Số Nhà: {selectedHousehold?.house_number}</p>
+        <p>Đường: {selectedHousehold?.street}</p>
+        <p>Phường/Xã: {selectedHousehold?.ward}</p>
+        <p>Quận/Huyện: {selectedHousehold?.district}</p>
+        <p>Chủ Hộ: {selectedHousehold?.residents?.find((resident) => resident.id === selectedHousehold?.head_of_household)?.full_name}</p>
+        <p>Kích Thước Hộ Gia Đình: {selectedHousehold?.household_size}</p>
+        <p>Ngày Đăng Ký Hộ Khẩu: {selectedHousehold?.date_of_registration}</p>
       </Card>
     );
   };
-  
+
   const renderResidentInfoPage = (resident) => {
     return (
-      <Card title={`Thông tin thành viên - ID: ${resident.id}`}>
+      <Card title={`Thông tin thành viên - ID: ${resident?.id}`}>
         {/* Hiển thị thông tin của thành viên trong gia đình */}
-        <p>Họ tên: {resident.full_name}</p>
-        <p>Giới tính: {resident.gender}</p>
-        <p>Ngày sinh: {resident.date_of_birth}</p>
-        <p>Nơi sinh: {resident.place_of_birth}</p>
-        <p>Dân tộc: {resident.ethnicity}</p>
-        <p>Nghề nghiệp: {resident.occupation}</p>
-        <p>Số CMND: {resident.id_card_number}</p>
-        <p>Ngày cấp CMND: {resident.date_of_issue}</p>
-        <p>Nơi cấp CMND: {resident.place_of_issue}</p>
-        <p>Tình trạng: {resident.status}</p>
-        <p>Ngày đăng ký: {resident.date_of_register}</p>
-        <p>Địa chỉ hiện tại: {resident.current_address}</p>
-        <p>Địa chỉ trước đó: {resident.previous_address}</p>
-        <p>Mối quan hệ với chủ hộ: {resident.relationship_with_head}</p>
+        <p>Họ tên: {resident?.full_name}</p>
+        <p>Giới tính: {resident?.gender}</p>
+        <p>Ngày sinh: {resident?.date_of_birth}</p>
+        <p>Nơi sinh: {resident?.place_of_birth}</p>
+        <p>Dân tộc: {resident?.ethnicity}</p>
+        <p>Nghề nghiệp: {resident?.occupation}</p>
+        <p>Số CMND: {resident?.id_card_number}</p>
+        <p>Ngày cấp CMND: {resident?.date_of_issue}</p>
+        <p>Nơi cấp CMND: {resident?.place_of_issue}</p>
+        <p>Tình trạng: {resident?.status}</p>
+        <p>Ngày đăng ký: {resident?.date_of_register}</p>
+        <p>Địa chỉ hiện tại: {resident?.current_address}</p>
+        <p>Địa chỉ trước đó: {resident?.previous_address}</p>
+        <p>Mối quan hệ với chủ hộ: {resident?.relationship_with_head}</p>
       </Card>
     );
   };
-  
 
   const handleNextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -60,33 +69,31 @@ const HouseholdDetailsModal = ({ selectedHousehold, modalVisible, handleModalClo
       footer={null}
       width={800}
     >
-      {selectedHousehold && (
-        <>
-          {currentPage === 0 ? (
-            // Trang thông tin hộ khẩu
-            <Step  description={renderHouseholdInfoPage()} />
-          ) : (
-            // Các trang thông tin thành viên trong gia đình
-            <Step
-              title={`Thành viên - ID: ${selectedHousehold.residents[currentPage - 1].id}`}
-              description={renderResidentInfoPage(selectedHousehold.residents[currentPage - 1])}
-            />
-          )}
+      <>
+        {currentPage === 0 ? (
+          // Trang thông tin hộ khẩu
+          <Step description={renderHouseholdInfoPage()} />
+        ) : (
+          // Các trang thông tin thành viên trong gia đình
+          <Step
+            title={`Thành viên - ID: ${residents[currentPage - 1]?.id}`}
+            description={renderResidentInfoPage(residents[currentPage - 1])}
+          />
+        )}
 
-          <div style={{ marginTop: "16px", textAlign: "right" }}>
-            {currentPage > 0 && (
-              <Button style={{ marginRight: "8px" }} onClick={handlePreviousPage}>
-                Trang trước
-              </Button>
-            )}
-            {currentPage < selectedHousehold.residents.length && (
-              <Button className="bg-blue-500" type="primary"  onClick={handleNextPage}>
-                Trang tiếp theo
-              </Button>
-            )}
-          </div>
-        </>
-      )}
+        <div style={{ marginTop: "16px", textAlign: "right" }}>
+          {currentPage > 0 && (
+            <Button style={{ marginRight: "8px" }} onClick={handlePreviousPage}>
+              Trang trước
+            </Button>
+          )}
+          {currentPage < residents.length && (
+            <Button className="bg-blue-500" type="primary" onClick={handleNextPage}>
+              Trang tiếp theo
+            </Button>
+          )}
+        </div>
+      </>
     </Modal>
   );
 };
